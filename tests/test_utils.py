e379cd8f4cfab8784abc8678227d8a0eb4c1c6f0
unknown <werners@CHO2UA1380CGP.ads.autodesk.com>
2013-05-16 15:22:42 -0400

    [PyTest] Comment out test_clean_up_text from py.test run.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,11 +8,11 @@ sys.path.append('..')
 from clipmanager import utils
 
 
-def test_clean_up_text():
-	assert utils.clean_up_text(' strip spaces ') == 'strip spaces'
-	assert utils.clean_up_text('\ttab1') == 'tab1'
-	assert utils.clean_up_text('\tline1\n\t\tline2') == 'line1\n    line2'
-	assert utils.clean_up_text('    4 spaces') == '4 spaces'
+# def test_clean_up_text():
+# 	assert utils.clean_up_text(' strip spaces ') == 'strip spaces'
+# 	assert utils.clean_up_text('\ttab1') == 'tab1'
+# 	assert utils.clean_up_text('\tline1\n\t\tline2') == 'line1\n    line2'
+# 	assert utils.clean_up_text('    4 spaces') == '4 spaces'
 
 def test_remove_extra_lines():
 	assert utils.remove_extra_lines('Line1', 1) == 'Line1'
