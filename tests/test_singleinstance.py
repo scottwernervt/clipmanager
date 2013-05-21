d81e63d9b37f8f3673a180abfa51f5eda539faad
Scott Werner <scott.werner.vt@gmail.com>
2013-05-21 08:38:11 -0400

    [PyTest] Merge platform specific test code into single method.
    - Prior implementation to test for single instance was complicated.
    - Made test_is_not_running the first test to run.
    - Eliminated testing call at bottom of script.

--- tests/test_singleinstance.py
+++ tests/test_singleinstance.py
@@ -11,41 +11,17 @@ sys.path.append('..')
 from clipmanager import singleinstance
 
 
-kill = False
-
-def _fake_app():
-	global kill
-
-	fake_instance = singleinstance.SingleInstance()
-	while kill:
-		time.sleep(1)
-
-
 class TestSingleInstance(object):
-
-	def test_is_running(self):
-		if sys.platform.startswith('win32'):
-			global kill
-			# Application is not running
-			app = Thread(target=_fake_app, args=())
-			app.start()
-
-			# Creating a 2nd instance will say it is already running
-			new_instance = singleinstance.SingleInstance()
-			assert (new_instance.is_running() == True)
-
-			# Exit thread
-			kill = False
-		else:
-			app_a = singleinstance.SingleInstance()
-			app_b = singleinstance.SingleInstance()
-			
-			assert (app_b.is_running() == True)
 
 	def test_is_not_running(self):
 		# Application is not running
 		instance = singleinstance.SingleInstance()
 		assert (instance.is_running() == False)
+		del instance
 
-test = TestSingleInstance()
-test.test_is_running()
+	def test_is_running(self):
+		app_a = singleinstance.SingleInstance()
+		app_b = singleinstance.SingleInstance()
+		
+		assert (app_b.is_running() == True)
+		del app_a, app_b
