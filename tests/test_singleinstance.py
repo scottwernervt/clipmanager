09bc176576fb3259d1730e700be2041aa6eabd33
Scott Werner <scott.werner.vt@gmail.com>
2013-05-20 20:48:10 -0400

    [PyTest] Support linux in single instance test.

--- tests/test_singleinstance.py
+++ tests/test_singleinstance.py
@@ -16,7 +16,7 @@ kill = False
 def _fake_app():
 	global kill
 
-	instance = singleinstance.SingleInstance()
+	fake_instance = singleinstance.SingleInstance()
 	while kill:
 		time.sleep(1)
 
@@ -24,20 +24,28 @@ def _fake_app():
 class TestSingleInstance(object):
 
 	def test_is_running(self):
-		global kill
-		# Application is not running
-		# assert (self.instance.is_running() == False)
-		app = Thread(target=_fake_app, args=())
-		app.start()
+		if sys.platform.startswith('win32'):
+			global kill
+			# Application is not running
+			app = Thread(target=_fake_app, args=())
+			app.start()
 
-		# Creating a 2nd instance will say it is already running
-		instance = singleinstance.SingleInstance()
-		assert (instance.is_running() == True)
+			# Creating a 2nd instance will say it is already running
+			new_instance = singleinstance.SingleInstance()
+			assert (new_instance.is_running() == True)
 
-		# Exit thread
-		kill = False
+			# Exit thread
+			kill = False
+		else:
+			app_a = singleinstance.SingleInstance()
+			app_b = singleinstance.SingleInstance()
+			
+			assert (app_b.is_running() == True)
 
 	def test_is_not_running(self):
 		# Application is not running
 		instance = singleinstance.SingleInstance()
 		assert (instance.is_running() == False)
+
+test = TestSingleInstance()
+test.test_is_running()
