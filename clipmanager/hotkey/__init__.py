558405814e8a74f5e8e5389b4984f98d7bbc9b40
Scott Werner <scott.werner.vt@gmail.com>
2018-02-02 17:36:34 -0500

    Make hotkey a module and automatically auto load hot key manager by OS.

--- clipmanager/hotkey/__init__.py
+++ clipmanager/hotkey/__init__.py
@@ -1,2 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os
+
+
+def initialize():
+    if os.name == 'nt':
+        pass
+        # from gui.hotkeys.win32.global_hotkey_win import GlobalHotkeyManagerWin
+        # return GlobalHotkeyManagerWin()
+    elif os.name == 'posix':
+        from clipmanager.hotkey.x11 import GlobalHotkeyManagerX11
+        return GlobalHotkeyManagerX11()
