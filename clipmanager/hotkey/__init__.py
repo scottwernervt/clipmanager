1bfa96f4822bb02600099249e0f3bdded4d9a3cc
Scott Werner <scott.werner.vt@gmail.com>
2018-02-05 12:18:37 -0500

    Load windwos global hot key manager if NT OS name.

--- clipmanager/hotkey/__init__.py
+++ clipmanager/hotkey/__init__.py
@@ -3,9 +3,8 @@ import os
 
 def initialize():
     if os.name == 'nt':
-        pass
-        # from gui.hotkeys.win32.global_hotkey_win import GlobalHotkeyManagerWin
-        # return GlobalHotkeyManagerWin()
+        from clipmanager.hotkey.win32 import GlobalHotkeyManagerWin
+        return GlobalHotkeyManagerWin()
     elif os.name == 'posix':
         from clipmanager.hotkey.x11 import GlobalHotkeyManagerX11
         return GlobalHotkeyManagerX11()
