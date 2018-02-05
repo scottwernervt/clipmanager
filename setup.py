fe50e7f0f825b5fa3b4cfa6dc926d8a1855d65c8
Scott Werner <scott.werner.vt@gmail.com>
2018-02-05 12:18:54 -0500

    Update installation requirements.

--- setup.py
+++ setup.py
@@ -1,38 +1,70 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python2
 
 import os
-import sys
+import re
 
-from setuptools import Command
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
-# Application specific data from package
-version = __import__('clipmanager').__version__
+ROOT = os.path.abspath(os.path.dirname(__file__))
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
 
+install_requires = ['PySide']
+
+if os.name == 'nt':
+    install_requires.append('win32process')
+elif os.name == 'posix':
+    install_requires.append('python-xlib')
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 'clipmanager', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+download_url = 'https://github.com/scottwernervt/clipmanager' \
+               'archive/%s.tar.gz' % get_version()
 
 setup(
-    scripts = ['bin/clipmanager'],
-    name = 'clipmanager',
-    version = version,
-    description = "Manage the system's clipboard history.",
-    license = 'BSD',
-    author = 'Scott Werner',
-    author_email = 'scott.werner.vt@gmail.com',
-    maintainer = 'Scott Werner',
-    maintainer_email = 'scott.werner.vt@gmail.com',
-    url = 'https://bitbucket.org/mercnet/clipmanager',
-    download_url = 'https://bitbucket.org/mercnet/clipmanager',
-    include_package_data = True,
-    platforms = ['unix', 'linux', 'win32'],
-    requires = ['PySide', 'keybinder'],
-    packages = ['clipmanager', 'clipmanager.paste', 'clipmanager.hotkey'],
-    package_data = {
+    name='clipmanager',
+    version=get_version(),
+    author='Scott Werner',
+    author_email='scott.werner.vt@gmail.com',
+    description="Manage the system's clipboard history.",
+    long_description=open('README.md').read(),
+    license='BSD',
+    platforms='Posix; Windows',
+    keywords=' '.join([
+        'clipboard',
+        'manager',
+        'history',
+    ]),
+    url='https://github.com/scottwernervt/clipmanager',
+    download_url=download_url,
+    scripts=['bin/clipmanager'],
+    install_requires=install_requires,
+    extras_require={
+        'tests': [
+            'pytest',  # MIT
+        ],
+        'windows': [
+            'cx_Freeze',  # PSF
+        ],
+    },
+    setup_requires=[
+        'pytest-runner',  # MIT
+    ],
+    tests_require=[
+        'pytest',  # MIT
+    ],
+    packages=find_packages(exclude=['contrib', 'tests*']),
+    include_package_data=True,
+    package_data={
         'clipmanager': ['*.txt'],
         'clipmanager': ['icons/*.png', 'icons/*.ico'],
     },
-    data_files = [('share/applications', ['clipmanager.desktop']),
-                  ('share/pixmaps', ['clipmanager/icons/clipmanager.png']),
-                  ('/etc/xdg/autostart', ['clipmanager-autostart.desktop'])]
+    data_files=[
+        ('share/applications', ['clipmanager.desktop']),
+        ('share/pixmaps', ['clipmanager/icons/clipmanager.png']),
+        ('/etc/xdg/autostart', ['clipmanager-autostart.desktop'])
+    ],
 )
