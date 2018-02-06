4dd5e619d161ffa8b91294abbfcdd2c43c47016b
Scott Werner <scott.werner.vt@gmail.com>
2018-02-06 16:30:20 -0500

    Add logging to debug on windows.

--- clipmanager/owner/win32.py
+++ clipmanager/owner/win32.py
@@ -63,9 +63,9 @@ def get_win32_owner():
     EnumWindows(EnumWindowsProc(foreach_window), 0)
 
     for window_hwnd, window_title in window_titles:
-        logging.info(window_hwnd + '|' + window_title)
+        logger.info(window_hwnd + '|' + window_title)
         if window_hwnd == owner_hwnd:
-            logging.info('we got a match')
+            logger.info('we got a match')
 
     # DWORD WINAPI GetWindowThreadProcessId(
     #   _In_       HWND hWnd,
