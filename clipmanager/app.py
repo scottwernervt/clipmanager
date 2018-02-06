973e68c3b29c9e17843343ddccfdbd3a690b2598
Scott Werner <scott.werner.vt@gmail.com>
2018-02-06 10:26:33 -0500

    Clean up logging, doc strings, and refactor aboutToQuit Qt signal.

--- clipmanager/app.py
+++ clipmanager/app.py
@@ -11,17 +11,9 @@ from clipmanager.defs import APP_DOMAIN, APP_NAME, APP_ORG, APP_VERSION
 from clipmanager.mainwindow import MainWindow
 from clipmanager.singleinstance import SingleInstance
 
-LOGGING_LEVELS = {
-    'critical': 'CRITICAL',
-    'error': 'ERROR',
-    'warning': 'WARNING',
-    'info': 'INFO',
-    'debug': 'DEBUG'
-}
-
 
 def setup_logging(logging_level):
-    log_file_path = '%s/%s.log' % (QDir.tempPath(), APP_NAME)
+    log_file_path = '%s/%s.log' % (QDir.tempPath(), APP_NAME.lower())
     dict_log_config = {
         'version': 1,
         'handlers': {
@@ -46,7 +38,8 @@ def setup_logging(logging_level):
         },
         'formatters': {
             'custom_format': {
-                'format': '%(asctime)s - %(levelname)s - %(module)s.%(funcName)s:%(lineno)s - %(message)s'
+                'format': '%(asctime)s - %(levelname)s - %(module)s.%('
+                          'funcName)s:%(lineno)s - %(message)s '
             }
         }
     }
@@ -57,9 +50,9 @@ class MainApp(QApplication):
     """Application event loop which spawns the main window."""
 
     def __init__(self, args):
-        """Initialize application properties and open main window.
+        """Initialize application and launch main window.
 
-        :param args: sys.argv
+        :param args: 'minimize' on launch
         :type args: list
         """
         super(MainApp, self).__init__(args)
@@ -69,7 +62,7 @@ class MainApp(QApplication):
         self.setApplicationName(APP_NAME)
         self.setApplicationVersion(APP_VERSION)
 
-        # do not exit if main window hidden
+        # prevent application from exiting if minimized "closed"
         self.setQuitOnLastWindowClosed(False)
 
         if 'minimize' in args:
@@ -77,35 +70,36 @@ class MainApp(QApplication):
         else:
             self.mw = MainWindow(minimize=False)
 
-        # Perform clean up actions when quit message signaled
-        self.connect(self, SIGNAL('aboutToQuit()'), self.quit)
+        self.connect(self, SIGNAL('aboutToQuit()'), self.about_to_quit)
 
     @Slot()
-    def quit(self):
-        """Cleanup application and copy clipboard data to OS clipboard.
+    def about_to_quit(self):
+        """Clean up and set clipboard contents to OS clipboard.
 
-        Makes a copy of the clipboard pointer data into the OS clipboard. The 
-        basic concept behind this is that by default copying something into the
-        clipboard only copies a reference/pointer to the source application. 
-        Then when another application wants to paste the data from the 
-        clipboard  it requests the data from the source application.
+        The basic concept behind this is that by default copying something
+        into the clipboard only copies a reference/pointer to the source
+        application. Then when another application wants to paste the data
+        from the clipboard it requests the data from the source application.
 
-        Args:
-            None
-
-        Returns:
-            None
+        :return: None
+        :rtype: None
         """
-        self.mw.destroy()
-
-        # Copy and remove pointer
         clipboard = QApplication.clipboard()
         event = QEvent(QEvent.Clipboard)
         QApplication.sendEvent(clipboard, event)
-        logging.info('Exiting...')
+
+        self.mw.destroy()
 
 
 def main(argv):
+    """Entry point for launching ClipManager.
+
+    :param argv:
+    :type argv:
+
+    :return:
+    :rtype:
+    """
     # Allow user to set a logging level if issues
     parser = optparse.OptionParser()
     parser.add_option('-l', '--logging-level', help='Logging level')
@@ -113,13 +107,11 @@ def main(argv):
     logging.debug(options)
     logging.debug(args)
 
-    # If user does not specify an option then lower will fail
-    try:
-        log_option = options.logging_level.lower()
-    except AttributeError:
-        log_option = options.logging_level
+    if options.logging_level:
+        logging_level = options.logging_level.upper()
+    else:
+        logging_level = 'INFO'
 
-    logging_level = LOGGING_LEVELS.get(log_option, 'INFO')
     setup_logging(logging_level)
 
     single_instance = SingleInstance()
@@ -128,10 +120,7 @@ def main(argv):
         return -1
 
     app = MainApp(argv)
-    run = app.exec_()
-
-    logging.info('Exit code: %s' % run)
-    return run
+    app.exec_()
 
 
 if __name__ == '__main__':
