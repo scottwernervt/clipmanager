7234ea8c275949a7f89eaca8da3d29c4faf90128
Scott Werner <scott.werner.vt@gmail.com>
2018-02-16 11:44:28 -0500

    Fix instance attribute key defined outside of __init__.

--- clipmanager/ui/dialogs/settings.py
+++ clipmanager/ui/dialogs/settings.py
@@ -35,52 +35,32 @@ class SettingsDialog(QDialog):
     def __init__(self, parent=None):
         super(SettingsDialog, self).__init__(parent)
 
-        self.setWindowIcon(get_icon('clipmanager.ico'))
         self.setWindowTitle('Settings')
+        self.setWindowIcon(get_icon('clipmanager.ico'))
         self.setAttribute(Qt.WA_DeleteOnClose)
 
-        self.setup_ui()
-
-    def setup_ui(self):
-        """Setup Settings Dialog UI.
-
-        TODO: Re-enable word_wrap widget when QDelegate Sizing issue fixed.
-
-        :return: None
-        :rtype: None
-        """
-        # Global hot key
         self.key_combo_edit = HotKeyEdit(self)
         self.key_combo_edit.setText(settings.get_global_hot_key())
 
-        # Number of lines to display
         self.line_count_spin = QSpinBox(self)
         self.line_count_spin.setRange(1, 10)
         self.line_count_spin.setValue(settings.get_lines_to_display())
 
-        # Where to open the dialog
         self.open_at_pos_combo = QComboBox(self)
         self.open_at_pos_combo.addItem('Mouse cursor', 0)
         self.open_at_pos_combo.addItem('Last position', 1)
         self.open_at_pos_combo.addItem('System tray', 2)
 
-        # Word wrap display text
-        self.word_wrap = QCheckBox('Word wrap')
-        self.word_wrap.setCheckState(_qcheckbox_state(settings.get_word_wrap()))
-
         global_form = QFormLayout()
         global_form.setFieldGrowthPolicy(QFormLayout.FieldsStayAtSizeHint)
         global_form.addRow('Global shortcut:', self.key_combo_edit)
         global_form.addRow('Open window at:', self.open_at_pos_combo)
-        # global_form.addRow('Maximum entries:', self.entries_edit)
-        # global_form.addRow('Expire after:', self.expire_edit)
         global_form.addRow('Lines to display:', self.line_count_spin)
 
-        # Manage History
-        self.paste_check = QCheckBox('Paste in active window after '
-                                     'selection')
+        self.paste_check = QCheckBox('Paste in active window after selection')
         self.paste_check.setCheckState(
-            _qcheckbox_state(settings.get_send_paste()))
+            _qcheckbox_state(settings.get_send_paste())
+        )
 
         self.entries_edit = QLineEdit(self)
         self.entries_edit.setText(str(settings.get_max_entries_value()))
@@ -102,35 +82,31 @@ class SettingsDialog(QDialog):
         manage_box.setAlignment(Qt.AlignLeft)
         manage_box.setLayout(manage_form)
 
-        # Ignore Applications
         ignore_box = QGroupBox('Ignore the following applications:')
         self.exclude_list = QLineEdit(self)
-        self.exclude_list.setPlaceholderText('KeePass.exe;binaryname')
+        self.exclude_list.setPlaceholderText('BinaryName;WindowTitle;')
         self.exclude_list.setText(settings.get_exclude())
 
-        # Create separate layout for ignore applications
         ignore_layout = QVBoxLayout()
         ignore_layout.addWidget(self.exclude_list)
         ignore_box.setLayout(ignore_layout)
-        # Save and Cancel
-        button_box = QDialogButtonBox(QDialogButtonBox.Save |
-                                      QDialogButtonBox.Cancel)
 
-        # Set main layout
+        dialog_button_box = QDialogButtonBox(
+            QDialogButtonBox.Save | QDialogButtonBox.Cancel)
+
         main_layout = QVBoxLayout(self)
         main_layout.addLayout(global_form)
         main_layout.addWidget(self.paste_check)
         main_layout.addWidget(manage_box)
-        # main_layout.addWidget(self.word_wrap)
         main_layout.addWidget(ignore_box)
-        main_layout.addWidget(button_box)
+        main_layout.addWidget(dialog_button_box)
         self.setLayout(main_layout)
 
-        # LINUX: I use Windows key to move windows with my wm
+        # X11: Give focus for window managers, e.g. i3.
         self.setFocus(Qt.PopupFocusReason)
 
-        button_box.accepted.connect(self.save)
-        button_box.rejected.connect(self.cancel)
+        dialog_button_box.accepted.connect(self.save)
+        dialog_button_box.rejected.connect(self.cancel)
 
     def save(self):
         """Save settings and and close the dialog.
@@ -140,16 +116,14 @@ class SettingsDialog(QDialog):
         """
         settings.set_global_hot_key(self.key_combo_edit.text())
         settings.set_lines_to_display(self.line_count_spin.value())
-        settings.set_word_wrap(self.word_wrap.isChecked())
         settings.set_send_paste(self.paste_check.isChecked())
         settings.set_exclude(self.exclude_list.text())
         settings.set_max_entries_value(self.entries_edit.text())
         settings.set_expire_value(self.expire_edit.value())
 
-        # Get data integer from combo box string
-        index = self.open_at_pos_combo.currentIndex()
-        userdata = self.open_at_pos_combo.itemData(index)
-        settings.set_open_window_at(userdata)
+        open_at_index = self.open_at_pos_combo.currentIndex()
+        open_at_value = self.open_at_pos_combo.itemData(open_at_index)
+        settings.set_open_window_at(open_at_value)
 
         settings.sync()
         self.done(True)
