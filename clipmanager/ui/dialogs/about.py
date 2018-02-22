3cb48363b4bc08aaf12e43f2b815939a1c2906c2
Scott Werner <scott.werner.vt@gmail.com>
2018-02-22 17:51:34 -0500

    Clean up layout and only list app homepage

--- clipmanager/ui/dialogs/about.py
+++ clipmanager/ui/dialogs/about.py
@@ -20,18 +20,10 @@ class AboutDialog(QDialog):
         app_name = QCoreApplication.applicationName()
         app_version = QCoreApplication.applicationVersion()
 
-        app_url = QLabel('<a href="%s">%s</a>' % (__url__, __url__))
-        app_url.setTextFormat(Qt.RichText)
-        app_url.setTextInteractionFlags(Qt.TextBrowserInteraction)
-        app_url.setOpenExternalLinks(True)
-
-        icons_url = QLabel(
-            '<a href="{0}">{0}</a>'.format(
-                'https://github.com/horst3180/arc-icon-theme')
-        )
-        icons_url.setTextFormat(Qt.RichText)
-        icons_url.setTextInteractionFlags(Qt.TextBrowserInteraction)
-        icons_url.setOpenExternalLinks(True)
+        app_homepage = QLabel('<a href="%s">%s</a>' % (__url__, __url__))
+        app_homepage.setTextFormat(Qt.RichText)
+        app_homepage.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        app_homepage.setOpenExternalLinks(True)
 
         self.button_box = QDialogButtonBox(QDialogButtonBox.Close)
         self.button_box.setFocus()
@@ -43,11 +35,9 @@ class AboutDialog(QDialog):
         layout.addWidget(QLabel(app_version), 1, 1)
         layout.addWidget(QLabel('License:'), 2, 0)
         layout.addWidget(QLabel(__license__), 2, 1)
-        layout.addWidget(QLabel('Url:'), 4, 0)
-        layout.addWidget(app_url, 4, 1)
-        layout.addWidget(QLabel('Icons:'), 5, 0)
-        layout.addWidget(icons_url, 5, 1)
-        layout.addWidget(self.button_box, 6, 0, 1, 4)
+        layout.addWidget(QLabel('Homepage:'), 4, 0)
+        layout.addWidget(app_homepage, 4, 1)
+        layout.addWidget(self.button_box, 5, 0, 1, 4)
         self.setLayout(layout)
 
         self.button_box.rejected.connect(self.close)
