09883103caeb6e7dd93ef2fba8ed79f53d5054d2
Scott Werner <scott.werner.vt@gmail.com>
2018-02-22 19:31:38 -0500

    Add new cxfreeze script

---
+++ setup_cxfreeze.py
@@ -0,0 +1,30 @@
+import os
+import re
+import sys
+
+from cx_Freeze import Executable, setup
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 'clipmanager', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages=[], excludes=[])
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+
+executables = [
+    Executable('clipmanager/app.py', base=base, targetName='clipmanager.exe')
+]
+
+setup(name='clipmanager',
+      version=get_version(),
+      description="Manage the system's clipboard history.",
+      options=dict(build_exe=buildOptions),
+      executables=executables)
