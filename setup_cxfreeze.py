7e5337b9ab0fb22ee0f09c7ddc9408575aac24e5
Scott Werner <scott.werner.vt@gmail.com>
2018-02-22 22:37:59 -0500

    Conform to PEP8 naming conventions

--- setup_cxfreeze.py
+++ setup_cxfreeze.py
@@ -7,26 +7,22 @@ from cx_Freeze import Executable, setup
 ROOT = os.path.abspath(os.path.dirname(__file__))
 VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
 
+python_install_dir, _ = os.path.split(sys.executable)
+site_packages_path = os.path.join(python_install_dir, 'Lib', 'site-packages')
+
 
 def get_version():
     init = open(os.path.join(ROOT, 'clipmanager', '__init__.py')).read()
     return VERSION_RE.search(init).group(1)
 
 
-python_install_dir, __ = os.path.split(sys.executable)
-site_packages_path = os.path.join(python_install_dir, 'Lib', 'site-packages')
-
-buildOptions = dict(
+build_options = dict(
     optimize=2,
     include_msvcr=False,
     include_files=[
         (
-            os.path.join(
-                site_packages_path,
-                'PySide',
-                'plugins',
-                'sqldrivers', 'qsqlite4.dll'
-            ),
+            os.path.join(site_packages_path,
+                         '\PySide\plugins\sqldrivers\qsqlite4.dll'),
             os.path.join('sqldrivers', 'qsqlite4.dll')
         ),
     ],
@@ -50,8 +46,10 @@ executables = [
     )
 ]
 
-setup(name='clipmanager',
-      version=get_version(),
-      description="Manage the system's clipboard history.",
-      options=dict(build_exe=buildOptions),
-      executables=executables)
+setup(
+    name='clipmanager',
+    version=get_version(),
+    description="Manage the system's clipboard history.",
+    options=dict(build_exe=build_options),
+    executables=executables
+)
